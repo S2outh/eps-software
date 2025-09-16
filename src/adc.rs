1e479771d6e64a120faf5c2638dfee5d2762a69f
eleocraft <eliseo.cailloux@gmx.de>
2025-09-16 17:37:11 +0200

    fixed precision with VREF measurement

--- src/adc.rs
+++ src/adc.rs
@@ -4,15 +4,20 @@ use defmt::info;
 use embassy_stm32::{adc::{Adc, AdcChannel, AnyAdcChannel, RxDma, SampleTime}, peripherals::ADC1};
 use calib::FactoryCalibratedValues;
 
-const VREF_VAL: f32 = 3.;
-const TS_1_VAL: f32 = 30.;
-const TS_2_VAL: f32 = 130.;
+// datasheet reference conditions
+const VREF_VAL_CV: i32 = 3_00;
+const TS_1_VAL_TENTH_DEG: i32 = 30_0;
+const TS_2_VAL_TENTH_DEG: i32 = 130_0;
+const TS_REL_VAL_TENTH_DEG: i32 = TS_2_VAL_TENTH_DEG - TS_1_VAL_TENTH_DEG;
 
-const V_DIVIDER_MULT: f32 = (100. + 10.) / 10.;
+const RAW_VALUE_RANGE_X100: i32 = 4096_00;
 
-const RAW_VALUE_RANGE: f32 = 4096.;
+// voltage divider
+const R1_OHM: i32 = 100;
+const R2_OHM: i32 = 10;
+const V_DIVIDER_MULT: i32 = (R1_OHM + R2_OHM) / R2_OHM;
 
-const BAT_1_CH_POS: usize = 0;
+const VBAT_1_CH_POS: usize = 0;
 const VTEMP_CH_POS: usize = 1;
 const VREF_CH_POS: usize = 2;
 
@@ -20,8 +25,6 @@ pub struct EPSAdc<'d, D: RxDma<ADC1>> {
     adc: Adc<'d, ADC1>,
     dma_channel: D,
     calib: FactoryCalibratedValues,
-    temp_mult: f32,
-    vref_numerator: f32,
     bat_1_channel: AnyAdcChannel<ADC1>,
     // bat_2_channel: AnyAdcChannel<ADC1>,
     // ext_channel: AnyAdcChannel<ADC1>,
@@ -33,8 +36,6 @@ impl<'d, D: RxDma<ADC1>> EPSAdc<'d, D> {
     pub fn new(mut adc: Adc<'d, ADC1>, dma_channel: D, bat_1_channel: AnyAdcChannel<ADC1>) -> Self {
         let calib = FactoryCalibratedValues::new();
 
-        info!("ts_cal_1 {} ts_cal_2 {} v_refint {}", calib.ts_cal_1, calib.ts_cal_2, calib.v_refint);
-
         adc.set_resolution(embassy_stm32::adc::Resolution::BITS12);
         // 16x oversampling
         adc.set_oversampling_ratio(0x03);
@@ -43,10 +44,8 @@ impl<'d, D: RxDma<ADC1>> EPSAdc<'d, D> {
 
         let vtemp_channel = adc.enable_temperature().degrade_adc();
         let vref_channel = adc.enable_vrefint().degrade_adc();
-        let temp_mult = (TS_2_VAL - TS_1_VAL) / (calib.ts_cal_2 - calib.ts_cal_1) as f32;
-        let vref_numerator = VREF_VAL * calib.v_refint as f32;
 
-        Self { adc, dma_channel, calib, temp_mult, vref_numerator, bat_1_channel, vtemp_channel, vref_channel }
+        Self { adc, dma_channel, calib, bat_1_channel, vtemp_channel, vref_channel }
     }
     pub async fn measure(&mut self) {
         let mut measurements = [0u16; 3];
@@ -62,15 +61,22 @@ impl<'d, D: RxDma<ADC1>> EPSAdc<'d, D> {
             &mut measurements
         ).await;
 
-        let vref_rel = self.calib.v_refint as f32 / measurements[VREF_CH_POS] as f32;
-        let vref = VREF_VAL * vref_rel;
-        info!("vref: {}", vref);
-
-        let temp = self.temp_mult * (vref_rel * measurements[VTEMP_CH_POS] as f32 - self.calib.ts_cal_1 as f32) + TS_1_VAL;
-        info!("temp: {}", temp);
-        info!("raw: {}", measurements[VTEMP_CH_POS]);
+        // Reference adc voltage for recurring calibration
+        let vref_measurement_x100 = 100 * measurements[VREF_CH_POS] as i32;
         
-        let vbat = (vref / RAW_VALUE_RANGE) * measurements[BAT_1_CH_POS] as f32;
-        info!("vbat: {}", vbat * V_DIVIDER_MULT);
+        // Measuring different adc channels
+        let vbat_1_measurement_x100 = 100 * measurements[VBAT_1_CH_POS] as i32;
+        let vtemp_measurement_x10 = 10 * measurements[VTEMP_CH_POS] as i32;
+
+        let vref_mv = VREF_VAL_CV * self.calib.v_refint_x100 / vref_measurement_x100;
+        info!("vref: {}", vref_mv);
+
+        let temp_calibrated_measurement = vtemp_measurement_x10 * self.calib.v_refint_x100 / vref_measurement_x100;
+        let temp_tenth_deg = TS_REL_VAL_TENTH_DEG * (temp_calibrated_measurement - self.calib.ts_cal_1_x10)
+            / self.calib.ts_cal_rel_x10 + TS_1_VAL_TENTH_DEG;
+        info!("temp: {}", temp_tenth_deg);
+        
+        let vbat_mv = vbat_1_measurement_x100 * vref_mv / RAW_VALUE_RANGE_X100;
+        info!("vbat: {}", vbat_mv * V_DIVIDER_MULT);
     }
 }
