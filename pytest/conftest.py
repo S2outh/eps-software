45c3226daf79210bc71fd62b25e817e893241eee
Raphael Hahn <rfha@gmx.de>
2026-01-30 21:01:42 +0100

    Fix test setup

--- pytest/conftest.py
+++ pytest/conftest.py
@@ -9,8 +9,7 @@ COORD = os.environ["LG_COORDINATOR"]
 PLACE = "flatsat"
 
 MATCH_LED   = "*/test-indicator-led/NetworkService/led"
-MATCH_OPEN_LST_1 = "*/open-lst-1-probers/NetworkService/probe-open-lst-1"
-Match_LOWER_SENSOR = "*/lower-sensor-probers/NetworkService/probe-lower-sensor"
+MATCH_EPS = "*eps-probers/NetworkService/probe-eps"
 
 def lg(*args, check=True):
     return subprocess.run(
@@ -25,8 +24,7 @@ def labgrid_session():
     lg("-p", PLACE, "delete", check=False)
     lg("-p", PLACE, "create", check=False)
     lg("-p", PLACE, "add-match", MATCH_LED, check=False)
-    lg("-p", PLACE, "add-match", MATCH_OPEN_LST_1, check=False)
-    lg("-p", PLACE, "add-match", Match_LOWER_SENSOR, check=False)
+    lg("-p", PLACE, "add-match", MATCH_EPS, check=False)
     print(lg("-p", PLACE, "show").stdout)
 
     # IMPORTANT: acquire before Environment() so RemotePlace can list resources
@@ -36,15 +34,13 @@ def labgrid_session():
         env = Environment("env.yaml")
         t = env.get_target("main")
         t.get_driver("HttpLedDriver")  # LED ON via driver activation
-        t.get_driver("ProbeRsDriver", name="probe-open-lst-1")
-        t.get_driver("ProbeRsDriver", name="probe-lower-sensor")
+        t.get_driver("ProbeRsDriver", name="probe-eps")
 
         yield t
     finally:
         # Optional: ensure driver hooks run before releasing place
         try:
-            t.get_driver("ProbeRsDriver", name="probe-open-lst-1").deactivate()
-            t.get_driver("ProbeRsDriver", name="probe-lower-sensor").deactivate()
+            t.get_driver("ProbeRsDriver", name="probe-eps").deactivate()
         except Exception:
             pass
         try:
